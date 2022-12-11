3f21ec6b534447823df4035e9f24d8a5eded1d46
anras5 <anras1filip@gmail.com>
2022-12-11 22:38:40 +0100

    Initial commit
    Add main java class, window class and drools file

---
+++ src/main/java/com/sample/Window.java
@@ -0,0 +1,28 @@
+package com.sample;
+
+import java.util.List;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public class Window {
+	public static JFrame frame;
+	
+	public static String updateFrame(String questionValue, List<String> options){
+		int index = JOptionPane.showOptionDialog(
+				null,
+				questionValue, "Answer this question:",
+                JOptionPane.DEFAULT_OPTION,
+                JOptionPane.INFORMATION_MESSAGE,
+                null, 
+                options.toArray(new String[options.size()]),
+                options.get(0)
+        );
+		
+		return options.get(index);
+	}
+	
+	public static void showMatch(String title){
+		JOptionPane.showMessageDialog(frame, "Best horror movie for you:\n" + title);
+	}
+}
